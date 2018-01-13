5581f3a883cd2b4ee9ac4bf2ff56b046d150d3b5
Martin Dreier <martin@martindreier.de>
2018-01-13 13:22:10 +0100

    Use Vue.js for UI translation

--- popup/popup.js
+++ popup/popup.js
@@ -1,2 +1,25 @@
+Vue.component('savetabs-separator', {
+    template: '<div class="panel-section-separator"></div>',
+});
+
+Vue.component('savetabs-list-item', {
+    template: '<div class="panel-list-item">\
+                 <div class="icon"><img v-bind:src="iconPath" /></div>\
+                 <div class="text" id="loadSavedTabs">{{ text }}</div>\
+                 <div class="text-shortcut"></div>\
+               </div>',
+    props: ['id', 'icon'],
+    data: function() {
+        return {
+            text: browser.i18n.getMessage(this.id),
+            iconPath: '../icons/' + this.icon
+        };
+    }
+})
+
+window.addEventListener('load', function () {
+    var vm = new Vue({
+        el: '#popup-content'
+    });
+})
 SaveTabs.createHooks(document);
-Translator.setupTranslation();
