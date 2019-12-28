7752f59402a4e387f14ae389fdf49e262b65006b
Martin Dreier <martin@martindreier.de>
2019-12-28 16:02:37 +0100

    Fix build script

--- scripts/build.js
+++ scripts/build.js
@@ -3,8 +3,13 @@ const webExt = require('web-ext').default;
 
 webExt.cmd.lint({
     sourceDir: path.resolve("./"),
+}, {
+    shouldExitProgram: false
 }).then(webExt.cmd.build({
     sourceDir: path.resolve("./"),
     artifactsDir: path.resolve("./web-ext-artifacts"),
-    overwriteDest: true
+    overwriteDest: true,
+    ignoreFiles: [
+        "scripts/"
+    ]
 }));
